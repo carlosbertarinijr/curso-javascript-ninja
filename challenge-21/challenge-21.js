40487b986246a120ee67d5406354732ee2a555b1
CABJr <bertarinicarlosjunior@gmail.com>
2019-10-03 11:22:46 -0300

    Projeto Timer

--- challenge-21/challenge-21.js
+++ challenge-21/challenge-21.js
@@ -1,3 +1,5 @@
+(function(win, doc){
+  'use strict';
 /*
 O desafio de hoje será um pequeno projeto: um cronômetro!
 As regras para criação do cronômetro são as seguintes:
@@ -14,4 +16,36 @@ Utilize o atributo data-js para nomear o campo e os botões. Você pode
 usar o nome que achar melhor, desde que ele seja semântico, ou seja, o nome
 dado ao elemento HTML deve definir o que o elemento é ou o que ele faz.
 */
-// ?
+  //Get(pegar) nos elementos do document, para adicionar os eventos
+  var $timer = doc.querySelector('[data-js="timer"]');
+  var $start = doc.querySelector('[data-js="start"]');
+  var $stop = doc.querySelector('[data-js="stop"]');
+  var $reset = doc.querySelector('[data-js="reset"]');
+  var interval; // variavel para pegar o ID setTimeout
+  //fim do GET
+
+  //adicionando os Eventos
+  $start.addEventListener('click', startTimer, false);
+  $stop.addEventListener('click', stopTimer, false);
+  $reset.addEventListener('click', resetTimer, false);
+  //fim de Adicionar eventos
+
+  //Funções para SET(Atribuir), os eventos que aconteceram no doc
+
+  function startTimer(){
+    $timer.value = +$timer.value + 1; //  O value o $timer é do tipo String, porém quando adiciono + antes do value automaticamente o JS  transformara em Number para podermos somar e não concatenar.
+    interval = setTimeout(startTimer, 1000); // SET(atribui) a variavel interval para GET(pegar) o ID do timer
+  };
+
+  function stopTimer(){
+    clearTimeout(interval);//Agora que eu GET(peguei) o ID do timer posso parar o timer, e continuar de onde parei.
+  };
+
+  function resetTimer(){
+    $timer.value = 0;
+    stopTimer();//como a função resetTimer é especificadamente para zerar o timer e parar o mesmo, eu não preciso duplicar o codigo de parar, eu simplismente invoco a função stopTimer após zerar o timer, excluindo  a ideia de duplicar o codigo
+  };
+
+  //fim das funções para Setar os eventos.
+
+})(window,document);
